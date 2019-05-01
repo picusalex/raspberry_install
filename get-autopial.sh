7052a18f5e142ae1986a9532e8e46aab82375be4
picusalex <31538145+picusalex@users.noreply.github.com>
2019-05-01 11:27:35 +0200

    Update get-autopial.sh

--- get-autopial.sh
+++ get-autopial.sh
@@ -14,7 +14,7 @@ RESET=`tput sgr0`
 echo -e "${BLUE}Welcome to the autopial installation process !!${RESET}"
 
 echo -e "${CYAN}Step 1: installation of mandatory dependencies${RESET}"
-sudo apt install supervisor redis-server python3 python3-dev
+sudo apt-get install -y supervisor redis-server mosquitto python3 python3-dev
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 
